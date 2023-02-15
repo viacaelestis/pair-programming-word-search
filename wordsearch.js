4f43e59613874e06b0dfd97b6efaa1a6adecf249
viacaelestis <affectuminflamma@gmail.com>
2023-02-15 09:42:25 -0700

    made word search functions

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
 const wordSearch = (letters, word) => { 
+    if (letters.length === 0) {
+        return false;
+    }
+
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    const verticalJoin = letters.length;
+      const numCols = letters[0].length;
+      for (let i = 0; i < numCols; i++) {
+        let col = '';
+        for (let j = 0; j < verticalJoin; j++) {
+          col += letters[j][i];
+        }
+        if (col.includes(word)) {
+            return true;
+        }
+    
 }
+return false;};
 
-module.exports = wordSearch
+
+module.exports = wordSearch;
+
+
